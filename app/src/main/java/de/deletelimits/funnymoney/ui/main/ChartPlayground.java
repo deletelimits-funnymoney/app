eda6d6ff26f3c4f84f9d7c9e4cf4a4208285d9b6
Hubert Grzeskowiak <dev@nemesis13.de>
2016-06-04 21:13:54 +0200

    graphs WIP

--- app/src/main/java/de/deletelimits/funnymoney/ui/main/ChartPlayground.java
+++ app/src/main/java/de/deletelimits/funnymoney/ui/main/ChartPlayground.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -41,8 +42,9 @@ public class ChartPlayground extends BaseFragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_chart_playground, container, false);
         chart = (BarChart) root.findViewById(R.id.chart);
+        setStyling();
+        chart.invalidate();
 
-        setData(12, 50);
         List<String> labels = new ArrayList<>();
         for (int i=1; i<31; i++) {
             labels.add(String.valueOf(i));
@@ -52,7 +54,8 @@ public class ChartPlayground extends BaseFragment {
             values.add((float) i*1000);
         }
 
-//        setData(labels, values);
+        setData(labels, values);
+        chart.invalidate();
         return root;
     }
 
@@ -66,7 +69,8 @@ public class ChartPlayground extends BaseFragment {
             barEntries.add(new BarEntry(values.get(i), i));
         }
         BarDataSet set1 = new BarDataSet(barEntries, dataSetName);
-        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        set1.setColor(R.color.primary);
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
         dataSets.add(set1);
         return dataSets;
     }
@@ -74,38 +78,17 @@ public class ChartPlayground extends BaseFragment {
     private void setStyling() {
         chart.setDrawGridBackground(false);
         chart.setDescription("");
+        chart.setDrawValueAboveBar(false);
+        chart.setTouchEnabled(false);
+        chart.setDrawValueAboveBar(false);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        chart.getAxisLeft().setEnabled(false);
     }
 
     public void setData(List<String> xLabels, List<Float> values) {
         BarData data = new BarData(xLabels, valuesToDatasets(values));
         chart.setData(data);
     }
-
-    private void setData(int count, float range) {
-
-        ArrayList<String> xVals = new ArrayList<String>();
-        for (int i = 0; i < count; i++) {
-            xVals.add(months[i % 12]);
-        }
-
-        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
-
-        for (int i = 0; i < count; i++) {
-            float mult = (range + 1);
-            float val = (float) (Math.random() * mult);
-            yVals1.add(new BarEntry(val, i));
-        }
-
-        BarDataSet set1 = new BarDataSet(yVals1, "DataSet");
-        set1.setBarSpacePercent(35f);
-        set1.setColors(ColorTemplate.MATERIAL_COLORS);
-
-        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-        dataSets.add(set1);
-
-        BarData data = new BarData(xVals, dataSets);
-
-        chart.setData(data);
-    }
-
 }
